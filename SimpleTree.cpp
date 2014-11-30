a13019dc6037e3d48e8350751ca6d39e423062a0
Jonathan Redmann <jpredmann@gmail.com>
2014-11-29 18:18:56 -0600

    strides made in utility function. needs gamestate array rewrite

--- SimpleTree.cpp
+++ SimpleTree.cpp
@@ -20,7 +20,7 @@ const pair<int,int> loc9 (2,2);
 
 const pair<int,int> locations[] = {loc1, loc2, loc3, loc4, loc5, loc6, loc7, loc8, loc9};
 
-int locationsTaken[] = {0,0,0,0,0,0,0,0,0}; // LocationsTaken. 
+char gameState[9]; // gameState. 
 
 class SimpleTree
 {
@@ -28,7 +28,7 @@ public:
 	Move move;
 
 	vector<SimpleTree*> children;
-	array<int,3> selected;
+	array<char,3> selected;
 
 	int size; // number of children
 
@@ -40,7 +40,7 @@ public:
 	Move* getNextMove(SimpleTree* root);
 
 	void buildTree(Move* move);
-	int calculateUtility();
+	int calculateUtility(char marker);
 	void print();
 	void printChildren();
 	static SimpleTree* maxNode(SimpleTree* a, SimpleTree* b);
@@ -55,13 +55,12 @@ public:
 
 	Move getMove();
 
-	bool hasTwo(array<int,3> a);
-	array<int,3> getRows(int row);
-	array<int,3> getColumns(int column);
-	array<int,3> getDiagonals(int diagonal);
+	char hasTwo(array<char,3> a);
+	array<char,3> getRows(int row);
+	array<char,3> getColumns(int column);
+	array<char,3> getDiagonals(int diagonal);
 
-	bool canWin(); 
-	bool canBlockRow();
+	char canWin(); 
 	bool canFork(); 
 	bool canBlockFork();
 	bool canCenter(); 
@@ -225,15 +224,18 @@ Move* SimpleTree::getNextMove(SimpleTree* root)
 	return nullptr;
 }
 
-int SimpleTree::calculateUtility()
+int SimpleTree::calculateUtility(char currentMarker)
 {
 	int value = 0;
 
-	if(canWin())
+	char possibleWin = canWin();
+
+
+	if(possibleWin != ' '  && possibleWin == currentMarker)  // can win
 	{
 		value = 8;
 	}
-	else if(canBlockRow())
+	else if(possibleWin != ' '  && possibleWin != currentMarker) // can block
 	{
 		value = 7;
 	}
@@ -274,92 +276,109 @@ void SimpleTree::branchToDecision()
 
 }
 
-// Returns true if two of three of the locations are taken
-bool SimpleTree::hasTwo(array<int,3> a)
+// Returns the marker if two of three of the locations are taken
+char SimpleTree::hasTwo(array<char,3> a)
 {
-	return a[0] + a[1] + a[2] == 2;
+	//return a[0] + a[1] + a[2] == 2;
+	return ' ';
 }
 
-array<int,3> SimpleTree::getRows(int row)
+array<char,3> SimpleTree::getRows(int row)
 {
 	if(row == 1)
 	{
-		selected[0] = locationsTaken[0];
-		selected[1] = locationsTaken[1];
-		selected[2] = locationsTaken[2];
+		selected[0] = gameState[0];
+		selected[1] = gameState[1];
+		selected[2] = gameState[2];
 	}
 	else if (row == 2)
 	{
-		selected[0] = locationsTaken[3];
-		selected[1] = locationsTaken[4];
-		selected[2] = locationsTaken[5];
+		selected[0] = gameState[3];
+		selected[1] = gameState[4];
+		selected[2] = gameState[5];
 	}
 	else if (row == 3)
 	{
-		selected[0] = locationsTaken[6];
-		selected[1] = locationsTaken[7];
-		selected[2] = locationsTaken[8];
+		selected[0] = gameState[6];
+		selected[1] = gameState[7];
+		selected[2] = gameState[8];
 	}
 	return selected;
 }
 
-array<int,3> SimpleTree::getColumns(int column)
+array<char,3> SimpleTree::getColumns(int column)
 {
 
 	if(column == 1)
 	{
-		selected[0] = locationsTaken[0];
-		selected[1] = locationsTaken[3];
-		selected[2] = locationsTaken[6];
+		selected[0] = gameState[0];
+		selected[1] = gameState[3];
+		selected[2] = gameState[6];
 	}
 	else if (column == 2)
 	{
-		selected[0] = locationsTaken[1];
-		selected[1] = locationsTaken[4];
-		selected[2] = locationsTaken[7];
+		selected[0] = gameState[1];
+		selected[1] = gameState[4];
+		selected[2] = gameState[7];
 	}
 	else if (column == 3)
 	{
-		selected[0] = locationsTaken[2];
-		selected[1] = locationsTaken[5];
-		selected[2] = locationsTaken[8];	}
+		selected[0] = gameState[2];
+		selected[1] = gameState[5];
+		selected[2] = gameState[8];
+	}
 
 	return selected;
 }
 
-array<int,3> SimpleTree::getDiagonals(int diagonal)
+array<char,3> SimpleTree::getDiagonals(int diagonal)
 {
 
 	if(diagonal == 1)
 	{
-		selected[0] = locationsTaken[0];
-		selected[1] = locationsTaken[4];
-		selected[2] = locationsTaken[8];
+		selected[0] = gameState[0];
+		selected[1] = gameState[4];
+		selected[2] = gameState[8];
 	}
 	else if (diagonal == 2)
 	{
-		selected[0] = locationsTaken[2];
-		selected[1] = locationsTaken[4];
-		selected[2] = locationsTaken[6];
+		selected[0] = gameState[2];
+		selected[1] = gameState[4];
+		selected[2] = gameState[6];
 	}
 
 	return selected;
 }
 
-bool SimpleTree::canWin() //If the player has two in a row, they can place a third to get three in a row.
-{
-	bool canWin = false;
+//void SimpleTree::setGameState(char gamestate[]); // 
 
-	canWin = hasTwo(getRows(1)) || hasTwo(getRows(2)) || hasTwo(getRows(3))  || 
-	hasTwo(getColumns(1)) || hasTwo(getColumns(2)) || hasTwo(getColumns(3)) ||
-	hasTwo(getDiagonals(1)) || hasTwo(getDiagonals(2));
+// If the player has two in a row, they can place a third to get three in a row.
+// Returns the possible winner if there is a winner.
+char SimpleTree::canWin() 
+{
+	char canWin = ' ';
+
+	int iterator = 1;
+
+	while(canWin == ' ' && iterator > 4)
+	{
+		canWin = hasTwo(getRows(iterator));
+		iterator++;
+	}
+
+	while(canWin == ' ' && iterator > 4)
+	{
+		canWin = hasTwo(getColumns(iterator));
+		iterator++;
+	}
+
+	while(canWin == ' ' && iterator < 3)
+	{
+		canWin = hasTwo(getDiagonals(iterator));
+		iterator++;
+	}
 
 	return canWin;	
-}
-
-bool SimpleTree::canBlockRow() //If the opponent has two in a row, the player must play the third themselves to block the opponent.
-{
-	return false;	
 }
 
 bool SimpleTree::canFork() //Create an opportunity where the player has two threats to win (two non-blocked lines of 2).
@@ -393,12 +412,6 @@ bool SimpleTree::canEmptyCorner() //The player plays in a corner square.
 bool SimpleTree::canEmptySide() // The player plays in a middle square on any of the 4 sides.
 {
 	return false;		
-}
-
-// 
-Move* SimpleTree::getWin()
-{
-	return nullptr;	
 }
 
 Move* SimpleTree::getBlockRow() 
