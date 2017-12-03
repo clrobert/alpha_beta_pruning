1ea6d89afa5850c64b1a0cf06af041dd02979d36
Chris Roberts <clrobert@uno.edu>
2017-12-02 23:22:40 -0600

    added node object

--- minimax.py
+++ minimax.py
@@ -63,13 +63,18 @@ def get_base_board():
     return [0] * 9
 
 
-def build_node(board, turn):
-    return {
-        "current_board": list(board),
-        "next_nodes": None,
-        "value": None,
-        "turn": turn,
-    }
+class Node:
+    def __init__(self, board, turn):
+        self.current_board = list(board)
+        self.next_nodes = None
+        self.value = None
+        self.turn = turn
+
+    def printself(self):
+        print("Turn %s:" % self.turn)
+        print("Value: %s" % self.value)
+        print(self.current_board)
+        print(self.next_nodes)
 
 
 def enumerate_all_choices(node, turn):
@@ -77,41 +82,42 @@ def enumerate_all_choices(node, turn):
 
     # Yes, iterate over the entire board. Optimize later.
     for i in range(0, 9):
-        new_node = build_node(node["current_board"], turn)
-        if new_node["current_board"][i] == 0:
-            new_node["current_board"][i] = PLAYERS[turn%2]
+        new_node = Node(node.current_board, turn)
+        if new_node.current_board[i] == 0:
+            new_node.current_board[i] = PLAYERS[turn%2]
             all_choices.append(new_node)
 
     return all_choices
 
 
 def enumerate_all_games(current_node, turn):
-    current_node['next_nodes'] = enumerate_all_choices(current_node, turn)
-    if current_node['next_nodes']:
-        for next_node in current_node['next_nodes']:
+    current_node.next_nodes = enumerate_all_choices(current_node, turn)
+    if current_node.next_nodes:
+        for next_node in current_node.next_nodes:
             enumerate_all_games(next_node, turn + 1)
 
 
-def evaluate_all_games(all_games):
-    for game in all_games:
-
-
 def minimax(node, maximizing_player):
-    if not node["next_nodes"]:
-        return node["value"]
+    if not node.next_nodes:
+        return node.value
 
     if maximizing_player:
         best_value = -1
-        for child_node in node["next_nodes"]:
+        for child_node in node.next_nodes:
             value = minimax(child_node, False)
             best_value = max(best_value, value)
         return best_value
     else:
         best_value = 1
-        for child_node in node["next_nodes"]:
+        for child_node in node.next_nodes:
             value = minimax(child_node, True)
             best_value = min(best_value, value)
         return best_value
+
+
+def evaluate_all_games(all_games):
+    for game in all_games:
+        pass
 
 
 WIN_VALUE = 1
@@ -119,9 +125,11 @@ LOSE_VALUE = -1
 PLAYERS = [1, -1]
 turn = 0
 
-root_node = build_node(get_base_board(), turn - 1)
+root_node = Node(get_base_board(), turn - 1)
 enumerate_all_games(root_node, turn)
 
-# Need to be able to traverse tree reasonably; probably need a node class.
-# Need to evaluate give all nodes a value.
+root_node.printself()
+import pdb; pdb.set_trace()
+
+# Need to give all nodes a value.
 # Need to implement turn taking for two players so a game can be produced.
